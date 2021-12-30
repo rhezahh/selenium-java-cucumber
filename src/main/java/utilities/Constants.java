89adab1436a49f55b0d76b5a9bcdf0f73c01b8a7
rhèza <rheza@mamiteam.com>
2021-12-30 17:19:11 +0800

    change url value

--- src/main/java/utilities/Constants.java
+++ src/main/java/utilities/Constants.java
@@ -2,7 +2,7 @@ package utilities;
 
 public class Constants
 {
-    public static final String JAMBU_URL = "https://jambu.kerupux.com/";
+    public static final String GOOGLE_URL = "https://google.com/";
     public static final int PAGELOAD_WAIT_DURATION = 1800;
     public static final int WEBDRIVER_WAIT_DURATION = 60;
     public static final int PAGEFACTORY_WAIT_DURATION = 10;
